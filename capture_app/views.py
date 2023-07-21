11a2697d73b9a0ff11ec40d1f54af77840f94458
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-07-21 20:44:05 +0100

    Update views.py

--- capture_app/views.py
+++ capture_app/views.py
@@ -4,4 +4,8 @@ from django_form import ContactForm
 
 def index(request):
     form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass
     return render(request, 'index.html', {'form': form})
